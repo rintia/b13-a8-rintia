25664fc36c775bca3b59f417784dc2c5536d2a2e
Rintia Islam <rintiaislam110@gmail.com>
2026-05-02 12:10:40 +0600

    navbar added

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,49 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+
+const Navbar = () => {
+  return (
+    <div className="border-b px-2">
+      <nav className=" flex justify-between items-center  py-3 max-w-7xl mx-auto w-full">
+        <div className="flex gap-2 items-center">
+        <Link href={"/"}>
+        <Image
+            src={"/logo2.png"}
+            alt="logo"
+            loading="eager"
+            width={100}
+            height={100}
+            className="object-cover h-auto w-auto"
+          />
+        </Link>
+       
+          
+        </div>
+
+        <ul className="flex items-center gap-5 text-sm">
+          <li>
+            <Link href={"/"}>Home</Link>
+          </li>
+          <li>
+            <Link href={"/all-books"}>All Books</Link>
+          </li>
+        
+          <li>
+            <Link href={"/my-profile"}>My Profile</Link>
+          </li>
+        </ul>
+
+        <div className="flex gap-4">
+          <ul className="flex items-center  text-sm">
+            <li>
+              <Link href={"/signin"}>LogIn</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
