aef8a6133751c5a090ae28206bf916bf84e6a3e9
Rintia Islam <rintiaislam110@gmail.com>
2026-05-03 22:13:08 +0600

    google auth added

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,4 +1,5 @@
 "use client";
+import { authClient } from "@/lib/auth-client";
 import { Check } from "@gravity-ui/icons";
 import {
   Button,
@@ -10,15 +11,45 @@ import {
   Label,
   TextField,
 } from "@heroui/react";
+import { useRouter } from "next/navigation";
+import { GrGoogle } from "react-icons/gr";
 
-export default function RegisterPage() {
+export default function SignUpPage() {
+
+    const router = useRouter()
+
   const onSubmit = async (e) => {
     e.preventDefault();
+
+    const name = e.target.name.value;
+    const image = e.target.image.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    const {data, error} = await authClient.signUp.email({
+        name,
+        email,
+        password,
+        image,
+    })
+    
+
+    console.log({data, error})
+
+    if(!error) {
+        router.push('/')
+    }
+
   };
+    const handleGoogleSignIn = async () => {
+    await authClient.signIn.social({
+        provider: 'google'
+    })
+  }
 
   return (
     <Card className="border mx-auto w-125 py-10 mt-5">
-      <h1 className="text-center text-2xl font-bold">Register</h1>
+      <h1 className="text-center text-2xl font-bold">Sign Up</h1>
 
       <Form className="flex w-96 mx-auto flex-col gap-4" onSubmit={onSubmit}>
         <TextField isRequired name="name" type="text">
@@ -87,6 +118,10 @@ export default function RegisterPage() {
           </Button>
         </div>
       </Form>
+          <p className="text-center">Or</p>
+
+      <Button onClick={handleGoogleSignIn} variant="outline" className={'w-full'}><GrGoogle/> Sign In With Google</Button>
+
     </Card>
   );
 }
