908e45b1f84f7c82f3c36927bd6efe3518ecc2b5
Rintia Islam <rintiaislam110@gmail.com>
2026-05-03 22:51:35 +0600

    profile page added and update functionality added

--- src/app/my-profile/page.jsx
+++ src/app/my-profile/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 
+import { UpdateUserModal } from "@/components/UpdateUserModal";
 import { authClient } from "@/lib/auth-client";
 import { Avatar, Card } from "@heroui/react";
 import { redirect } from "next/navigation";
@@ -28,7 +29,7 @@ const ProfilePage = () => {
         <h2 className="text-xl font-bold">{user?.name}</h2>
         <p className="text-muted">{user?.email}</p>
 
-       
+       <UpdateUserModal/>
       </Card>
     </div>
   );
